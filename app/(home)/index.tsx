b0760e7ab0e0b8ab5f4b1e9ea397de2d7ea9058d
jinseok <jsseo@anchors-biz.com>
2024-12-29 13:02:15 +0900

    connect eas

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,10 +1,14 @@
 import Example from "@/components/DialogExample";
 import { HapticTab } from "@/components/HapticTab";
+import { Text } from "@/components/ui/text";
+import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 
 export default function HomeScreen() {
+  const { t } = useTranslation();
   return (
     <View>
+      <Text>{t("cart.discount")}</Text>
       <HapticTab>
         <Example />
       </HapticTab>
